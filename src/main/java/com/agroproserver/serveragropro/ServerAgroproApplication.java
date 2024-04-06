7022f52467b1eef31a9e46aea094bab2120f8653
cristian <cristiansanchezn14@gmail.com>
2024-04-06 13:59:17 +0200

    proyecto inicial

---
+++ src/main/java/com/agroproserver/serveragropro/ServerAgroproApplication.java
@@ -0,0 +1,13 @@
+package com.agroproserver.serveragropro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerAgroproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerAgroproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agroproserver/serveragropro/ServerAgroproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agroproserver.serveragropro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerAgroproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
